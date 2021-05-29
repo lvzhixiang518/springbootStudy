39c7c5ff03e8f9a12cdbbe14a43b0dea9f582f1e
lvzhixiang518 <962745314@qq.com>
2021-05-29 21:58:47 +0800

    springboot都一次提交 add by lzx 2021年5月29日21:58:35

---
+++ src/main/java/com/lzx/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.lzx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/test")
+    //@ResponseBody
+    public String hello(Model model){
+        model.addAttribute("msg","<h1>nihao a</h1>");
+        model.addAttribute("users", Arrays.asList("lzx","zhangsan"));
+        return "test";
+    }
+}
